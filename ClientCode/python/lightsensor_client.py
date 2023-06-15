c35e506aa727f0ec46896924e8ba6a2a497a3d45
André <52675847+Adrm00@users.noreply.github.com>
2023-06-15 18:17:56 -0300

    Update lightsensor_client.py
    correção

--- ClientCode/python/lightsensor_client.py
+++ ClientCode/python/lightsensor_client.py
@@ -12,7 +12,7 @@ from const import *
 def run():
     with grpc.insecure_channel(GRPC_SERVER+':'+GRPC_PORT) as channel:
         stub = iot_service_pb2_grpc.IoTServiceStub(channel)
-        response = stub.SayLightLevel(iot_service_pb2.LightLevelRequest(sensorName='my_sensor'),user=sys.argv[1],password=sys.argv[2])
+        response = stub.SayLightLevel(iot_service_pb2.LightLevelRequest(sensorName='my_sensor',user=sys.argv[1],password=sys.argv[2])
 
     print("Light level received: " + response.lightLevel)
 
