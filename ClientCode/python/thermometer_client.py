9a67b50b266ccfc29cdf63592cda6b86be7404cf
André <52675847+Adrm00@users.noreply.github.com>
2023-06-15 18:18:23 -0300

    Update thermometer_client.py
    Adicionada autenticação

--- ClientCode/python/thermometer_client.py
+++ ClientCode/python/thermometer_client.py
@@ -12,7 +12,7 @@ from const import *
 def run():
     with grpc.insecure_channel(GRPC_SERVER+':'+GRPC_PORT) as channel:
         stub = iot_service_pb2_grpc.IoTServiceStub(channel)
-        response = stub.SayTemperature(iot_service_pb2.TemperatureRequest(sensorName='my_sensor'))
+        response = stub.SayTemperature(iot_service_pb2.TemperatureRequest(sensorName='my_sensor',user=sys.argv[1],password=sys.argv[2]))
 
     print("Temperature received: " + response.temperature)
 
