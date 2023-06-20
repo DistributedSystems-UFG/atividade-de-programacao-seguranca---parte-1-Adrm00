222ef3481e55d5a393a34c5de64b7b9c7ac5ed48
André <52675847+Adrm00@users.noreply.github.com>
2023-06-20 14:46:50 -0300

    Update thermometer_client.py

--- ClientCode/python/thermometer_client.py
+++ ClientCode/python/thermometer_client.py
@@ -13,7 +13,7 @@ from const import *
 def run():
     with grpc.insecure_channel(GRPC_SERVER+':'+GRPC_PORT) as channel:
         stub = iot_service_pb2_grpc.IoTServiceStub(channel)
-        response = stub.SayTemperature(iot_service_pb2.TemperatureRequest(sensorName='my_sensor',user=sys.argv[1],password=sys.argv[2]))
+        response = stub.SayTemperature(iot_service_pb2.TemperatureRequest(sensorName='my_sensor',username_auth=sys.argv[1],password_auth=sys.argv[2]))
 
     if response:
         print("Temperature received: " + response.temperature)
